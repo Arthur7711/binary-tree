d8dad362cb36ca70489ccd38a24f9231cb9e3643
Arthur <newartroll11@gmail.com>
2021-09-24 10:14:26 +0400

    feat:(add tree)

--- index.js
+++ index.js
@@ -1,2 +1,102 @@
-console.log("lola");
 // https://jrsinclair.com/articles/2019/functional-js-traversing-trees-with-recursive-reduce/
+
+const menu = {
+  type: "title",
+  text: "Menu",
+  children: [
+    {
+      type: "title",
+      text: "Works of George Macdonald",
+    },
+    {
+      type: "link",
+      href: "/books",
+      text: "Books",
+      children: [
+        {
+          type: "link",
+          href: "/books/sir-gibbie",
+          text: "Sir Gibbie",
+        },
+        {
+          type: "link",
+          href: "/books/at-the-back-of-the-north-wind",
+          text: "At the Back of the North Wind",
+        },
+        {
+          type: "link",
+          href: "/books/the-princess-and-the-goblin",
+          text: "The Princess and the Goblin",
+        },
+      ],
+    },
+    {
+      type: "link",
+      href: "/poetry",
+      text: "Poems",
+    },
+    {
+      type: "link",
+      href: "/essays",
+      text: "Essays",
+      children: [
+        {
+          type: "link",
+          href: "/essays/the-fantastic-imagination",
+          text: "The Fantastic Imagination",
+        },
+        {
+          type: "link",
+          href: "/essays/the-new-name",
+          text: "The New Name",
+        },
+      ],
+    },
+    {
+      type: "title",
+      text: "Our Community",
+    },
+    {
+      type: "link",
+      href: "/about",
+      text: "About us",
+      children: [
+        {
+          type: "link",
+          href: "/about/membership",
+          text: "Community membership",
+        },
+        {
+          type: "link",
+          href: "/about/sponsorship",
+          text: "Community sponsorship",
+          children: [
+            {
+              type: "link",
+              href: "/about/sponsorship/patreon",
+              text: "Our Patreon",
+            },
+            {
+              type: "link",
+              href: "/about/sponsorship/endowments",
+              text: "Endowments",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      type: "link",
+      href: "/forum",
+      text: "Forum",
+    },
+    {
+      type: "link",
+      href: "https://www.facebook.com/groups/GeorgeMacDonaldCommunity/",
+      text: "Facebook Group",
+    },
+  ],
+};
+
+
+ 
